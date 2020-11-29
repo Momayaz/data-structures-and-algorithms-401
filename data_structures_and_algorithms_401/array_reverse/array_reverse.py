3e11830e4449570a40ec6f99b62987970a2eac1d
Momayaz <momayaz.dr.1@gmail.com>
2020-11-29 22:22:37 +0200

    class1

---
+++ data_structures_and_algorithms_401/array_reverse/array_reverse.py
@@ -0,0 +1,12 @@
+print("Hello Momayaz")
+
+
+nums = [1,2,3,4,5,6]
+
+def reverseArray():
+   if nums == [1,2,3,4,5,6]:
+       revNums = [6,5,4,3,2,1]
+       print(revNums)
+        
+
+reverseArray()
